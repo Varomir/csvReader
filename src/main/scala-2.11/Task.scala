75752bde2f434d4630117a8da056034277c80bd8
Vadym Feshchenko <Vadym.Feshchenko@playtech.com>
2016-09-29 12:47:56 +0300

    Fix literal error

--- src/main/scala-2.11/Task.scala
+++ src/main/scala-2.11/Task.scala
@@ -1,5 +1,5 @@
 import com.typesafe.scalalogging.LazyLogging
 
 object Main extends App with LazyLogging {
-git  logger.warn("Hello Scala and SBT world :)");
+  logger.warn("Hello Scala and SBT world :)");
 }
