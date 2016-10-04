492a9c91a61f4779add86c8573a1cac649770f6a
Vadim Feshchenko <vadim0961267452@gmail.com>
2016-10-05 01:04:45 +0300

    add specifiction stub

---
+++ src/test/scala-2.11/TaskInSpec.scala
@@ -0,0 +1,9 @@
+import org.specs2.Specification
+
+class TaskInSpec extends Specification { def is = s2"""
+
+ This is my specification for Task
+   it is working                 $ok
+   really working!               $ok
+                                 """
+}
